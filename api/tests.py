9d8773220139596d33d59b1630e8b9f5e25595a0
Mutia Rahmatun Husna <mutia.rahmatun@ui.ac.id>
2020-12-16 12:47:34 +0700

    remove unused line

--- api/tests.py
+++ api/tests.py
@@ -7,7 +7,6 @@ class GameTestCase(TestCase):
     
     def setUp(self):
         Game.objects.create(name="game1", original_price=15000)
-        # Game.objects.create(name="cat", sound="meow")
 
     def test_game_name(self):
         game1 = Game.objects.get(name="game1")
