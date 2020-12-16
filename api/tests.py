27402fffb2fe73a038642316e1d57cc6323efe28
Mutia Rahmatun Husna <mutia.rahmatun@ui.ac.id>
2020-12-16 12:52:32 +0700

    create test for new price

--- api/tests.py
+++ api/tests.py
@@ -7,7 +7,12 @@ class GameTestCase(TestCase):
     
     def setUp(self):
         Game.objects.create(name="game1", original_price=15000)
+        Game.objects.create(name="game2", original_price=15000, discount=2500)
 
     def test_game_name(self):
         game1 = Game.objects.get(name="game1")
         self.assertEqual(game1.__str__(), "game1")
+
+    def test_new_price(self):
+        game2 = Game.objects.get(name="game2")
+        self.assertEqual(15000-2500, game2.original_price - game2.discount)
