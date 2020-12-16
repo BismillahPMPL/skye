351bddb0fc5b49e40fa1ed9ec402d507176432df
Mutia Rahmatun Husna <mutia.rahmatun@ui.ac.id>
2020-12-16 12:46:22 +0700

    create game test

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Game
 
 # Create your tests here.
+
+class GameTestCase(TestCase):
+    
+    def setUp(self):
+        Game.objects.create(name="game1", original_price=15000)
+        # Game.objects.create(name="cat", sound="meow")
+
+    def test_game_name(self):
+        game1 = Game.objects.get(name="game1")
+        self.assertEqual(game1.__str__(), "game1")
